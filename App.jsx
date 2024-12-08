804b57718088b9eae6e8a5a91f08820f8b8758c1
vulla sai kiran kumar <vullasaikiran@gmail.com>
2024-12-08 23:57:46 +0530

    Removed unused code

--- App.jsx
+++ App.jsx
@@ -1,4 +1,3 @@
-import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import MainStack from './rootNavigation/MainStack';
@@ -16,5 +15,3 @@ const App = () => {
 };
 
 export default App;
-
-const styles = StyleSheet.create({});
